3d34b2bb2220e7a1c9252b7cf9dfe9dd8b2a8140
dwestland <don@westland.net>
2021-12-17 17:09:14 -0800

    Add cam API for each state

---
+++ pages/api/cams/california.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '.prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    try {
+      const cams = await prisma.cams.findMany({
+        where: {
+          state: 'CA',
+        },
+        select: {
+          id: true,
+          title: true,
+          // slug: true,
+          webcamUrl: true,
+          oldImageUrl: true,
+          description: true,
+          country: true,
+          state: true,
+          area: true,
+          subArea: true,
+
+          author: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+            },
+          },
+        },
+      })
+      res.status(200).json({ cams })
+    } catch (err) {
+      console.log(err)
+      res.status(403).json({ err: 'Error occurred.' })
+    }
+    return null
+  }
+
+  return res.status(405).json({ message: 'Method not allowed' })
+}
