aacad9fdcc8da3e1efe7cad7e208747699ed903c
dwestland <don@westland.net>
2021-12-19 14:49:12 -0800

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaExclamationTriangle } from 'react-icons/fa'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import styles from '@/styles/404.module.scss'
+
+export default function NotFoundPage() {
+  console.log('boom')
+
+  return (
+    <Layout title="Page Not Found">
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle /> Page not Found
+        </h1>
+        <h4>404 Error</h4>
+        <Link href="/">Go Back Home</Link>
+      </div>
+    </Layout>
+  )
+}
