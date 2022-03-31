c255e9ce5c71919e5586d45929baff18021439f1
dwestland <don@westland.net>
2022-03-31 08:05:45 -0700

    Add search API

---
+++ pages/api/cams/search.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '.prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  console.log(
+    '%c req.body.search.term ',
+    'background: red; color: white',
+    req.body.search.term
+  )
+
+  try {
+    const articles = await prisma.cams.findMany({
+      where: {
+        description: { contains: req.body.search.term, mode: 'insensitive' },
+
+        // title: { contains: req.body.search.term, mode: 'insensitive' },
+      },
+    })
+
+    res.status(200).json({ articles })
+  } catch (err) {
+    console.log(err)
+    res.status(403).json({ err: 'Error occurred.' })
+  }
+  return null
+}
