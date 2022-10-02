4acb8ec614d5ad3c5eb3dfdd566e30c5194fad90
dwestland <don@westland.net>
2022-10-02 08:39:09 -0700

    Add map layers

--- src/components/CamsMap.tsx
+++ src/components/CamsMap.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  LayersControl,
+} from 'react-leaflet'
 // import MarkerClusterGroup from 'react-leaflet-markercluster'
 // import 'react-leaflet-markercluster/dist/styles.min.css'
 
@@ -35,10 +41,39 @@ const CamsMap = ({ vectors }: Props) => {
       gestureHandling
       boundsOptions={{ padding: [50, 50] }}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="Watercolor">
+          <TileLayer
+            attribution='&copy; <a href="http://stamen.com">Stamen Design</a> contributors'
+            url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+          />
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer checked name="Street Map">
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+          /> */}
+        </LayersControl.BaseLayer>
+
+        <LayersControl.BaseLayer name="Satellite">
+          <TileLayer
+            attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Nat Geo">
+          <TileLayer
+            attribution="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
+          />
+        </LayersControl.BaseLayer>
+      </LayersControl>
+      <Marker position={[33.9765, -118.4483]} />
       {/* <MarkerClusterGroup> */}
       {vectors.map((vector) => (
         <Marker key={vector.name} position={[vector.lat, vector.lng]}>
