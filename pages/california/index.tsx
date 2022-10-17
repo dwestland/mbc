8ae572ed56b84e93a3fd41a73f776ad52c2725cf
dwestland <don@westland.net>
2022-10-16 19:14:59 -0700

    Add California and Florida pages

---
+++ pages/california/index.tsx
@@ -0,0 +1,246 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import Link from 'next/link'
+import CamItem from '@/components/CamItem'
+import AdLeaderboard from '@/components/AdLeaderboard'
+
+interface WebcamProps {
+  californiaCams: { title: string }[]
+}
+
+interface Cams {
+  id: number
+  title: string
+  webcamUrl: string
+  imageName: string
+  description: string
+  country: string
+  state: string
+  area: string
+  subarea: string
+  lat: number
+  lng: number
+  topCam: boolean
+  mbcHosted: boolean
+}
+
+const CaliforniaPage = ({ californiaCams }) => {
+  const emoji = <span style={{ fontSize: '40px' }}> 🌴 </span>
+  const sanDiegoCams = () => {
+    const cams = californiaCams.cams.filter((cam) => cam.area === 'San Diego')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const losAngelesCams = () => {
+    const cams = californiaCams.cams.filter((cam) => cam.area === 'Los Angeles')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const centralCoastCams = () => {
+    const cams = californiaCams.cams.filter(
+      (cam) => cam.area === 'Central Coast'
+    )
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const sanFranciscoCams = () => {
+    const cams = californiaCams.cams.filter(
+      (cam) => cam.area === 'San Francisco'
+    )
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  return (
+    <Layout
+      documentTitle="California Beach Cams - Webcams in San Diego, Los Angeles and San Francisco"
+      documentDescription="Best Beach Cams and Surf Cams in California featuring webcams in San Diego, Los Angeles, Central Coast and San Francisco and much more"
+    >
+      <div className="layout">
+        <h1>California Webcams</h1>
+        <h2>
+          <a href="/california/san-diego/">San Diego</a>
+          &nbsp;<span>{emoji}</span>
+          &nbsp;
+          <a href="/california/los-angeles/">Los Angeles</a>
+          &nbsp;<span>{emoji}</span>
+          &nbsp;
+          <a href="/california/central-coast/">Central Coast</a>
+          &nbsp;<span>{emoji}</span>
+          &nbsp;
+          <a href="/california/san-francisco/">San Francisco</a>
+        </h2>
+        <p>
+          California is known as the Golden State. Its sunny weather, varied
+          landscape and the allure of fame and fortune have enticed millions of
+          Americans to "go west." On this popular website you will find a series
+          of live streaming webcams from all along this famous West Coast. From
+          San Diego in the southern tip of California, up through Los Angeles
+          and Santa Barbara and then through Monterey and San Francisco, you
+          will see exciting live images of beaches, boardwalks, bays and even
+          marine life. These images will most likely either remind you of a fond
+          memory or make you want to trip out to see some of these exciting
+          destinations. Either way, enjoy the view!
+        </p>
+        <AdLeaderboard />
+        <h2>
+          <a href="/california/san-diego/">San Diego Beach Cams</a>
+        </h2>
+        <p>
+          San Diego, known for its year-round sunshine and balmy temperatures,
+          is a great place for some fun-in-the-sun. Click on{' '}
+          <a href="/california/san-diego/">San Diego Beach Cams</a> and check
+          out these live, streaming video web cams of the Del Mar, La Jolla/
+          Torrey Pines, San Diego Bay and Marina, Ocean Beach and Imperial Beach
+          areas. You'll then see why San Diego is one of the most popular
+          vacation spots in the U.S.A.
+        </p>
+        <div className="cam-container">
+          {sanDiegoCams()}
+          <div className="more-cams">
+            <Link href="/california/san-diego">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>SAN DIEGO</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/california/los-angeles">Los Angeles Beach Cams</Link>
+        </h2>
+        <p>
+          Los Angeles is home to many of the mainland's top beaches. Click on{' '}
+          <a href="/california/los-angeles/">Los Angeles Beach Cams</a> to find
+          streaming video webcams of such LA hot spots as Venice Beach, Santa
+          Monica Pier, Hermosa Beach, Newport Beach, Laguna and even Catalina
+          Island. Check out the great weather and see what's happening in the LA
+          scene.
+        </p>
+        <div className="cam-container">
+          {losAngelesCams()}
+          <div className="more-cams">
+            <Link href="/california/los-angeles">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>LOS ANGELES</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/california/central-coast">Cental Coast Beach Cams</Link>
+        </h2>
+        <p>
+          Along the west coast between Los Angeles and San Francisco is a series
+          of terrific resort towns. Many of them are weekend getaways for
+          Angelinos. Click on{' '}
+          <a href="/california/central-coast/">Central Coast Beach Cams</a> to
+          see live, streaming video of these areas. It starts at Ventura and
+          Santa Barbara, and then goes up the coast to Baywood and San Simeon,
+          and finally to Big Sur, Pebble Beach and Monterey. These webcams will
+          probably make you want to get in the car and drive up the coast along
+          scenic Highway 1.
+        </p>
+        <div className="cam-container">
+          {centralCoastCams()}
+          <div className="more-cams">
+            <Link href="/california/central-coast">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>CENTRAL COAST</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/california/san-francisco">
+            San Francisco &amp; Beach Northern California Cams
+          </Link>
+        </h2>
+        <p>
+          San Francisco is known as the "City by the Bay." This worldly,
+          seductive city is full of culture, magnificent sights and outstanding
+          food. Its famous sights include the Golden Gate Bridge, cable cars and
+          steep rolling hills overlooking the bay and ocean. See the web cams at{' '}
+          <a href="/california/san-francisco/">San Francisco Beach Cams</a> to
+          view streaming live pictures of this incredible area, including San
+          Francisco Bay, The Golden Gate Bridge and Alcatraz Island.
+        </p>
+        <div className="cam-container">
+          {sanFranciscoCams()}
+          <div className="more-cams">
+            <Link href="/california/san-francisco">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>SAN FRAN</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const moreCamsRes = await fetch(
+    `${process.env.NEXT_PUBLIC_API}/cams/california`
+  )
+  const californiaCams: WebcamProps = await moreCamsRes.json()
+
+  return {
+    props: {
+      californiaCams,
+    },
+  }
+}
+
+export default CaliforniaPage
---
+++ pages/florida/index.tsx
@@ -0,0 +1,293 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import Link from 'next/link'
+import CamItem from '@/components/CamItem'
+import AdLeaderboard from '@/components/AdLeaderboard'
+
+interface WebcamProps {
+  floridaCams: { title: string }[]
+}
+
+interface Cams {
+  id: number
+  title: string
+  webcamUrl: string
+  imageName: string
+  description: string
+  country: string
+  state: string
+  area: string
+  subarea: string
+  lat: number
+  lng: number
+  topCam: boolean
+  mbcHosted: boolean
+}
+
+const Page = ({ floridaCams }) => {
+  const panhandleCams = () => {
+    const cams = floridaCams.cams.filter((cam) => cam.area === 'Panhandle')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const northEastCams = () => {
+    const cams = floridaCams.cams.filter((cam) => cam.area === 'Northeast')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const eastCentralCams = () => {
+    const cams = floridaCams.cams.filter((cam) => cam.area === 'East Central')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const miamiBeachCams = () => {
+    const cams = floridaCams.cams.filter((cam) => cam.area === 'Miami Beach')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+  const souteastKeysCams = () => {
+    const cams = floridaCams.cams.filter(
+      (cam) => cam.area === 'South East Florida and The Keys'
+    )
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  const gulfCoastCams = () => {
+    const cams = floridaCams.cams.filter((cam) => cam.area === 'Gulf Coast')
+    const result = cams.map((cam: Cams, idx) => {
+      if (idx < 7) {
+        return <CamItem key={cam.id} cam={cam} />
+      }
+      return null
+    })
+    return result
+  }
+
+  return (
+    <Layout
+      documentTitle="MyBeachCams.com - Webcams of Hawaii, Florida and California"
+      documentDescription="Best Web Cams and Surf Cams in Hawaii, Florida and California and and local information about Miami, Los Angles, Miami, Northeast, San Francisco, Kauai and Fort Lauderdale"
+    >
+      <div className="layout">
+        <h1>Florida Beach Cams</h1>
+        <h3>Live Webcams from Florida</h3>
+        <h2>
+          <a href="/florida/miami/">Miami</a> -{' '}
+          <a href="/florida/southeast-keys/#fort-lauderdale-beach-cams">
+            Fort Lauderdale
+          </a>{' '}
+          -{' '}
+          <a href="/florida/east-central/#daytona-beach-cams">Daytona Beach</a>{' '}
+          -{' '}
+          <a href="/florida/southeast-keys/#florida-keys-beach-cams">
+            Florida Keys
+          </a>{' '}
+          - <a href="/florida/panhandle/#panama-city-beach-cams">Panama City</a>
+        </h2>
+        <p>
+          Florida has sunny, warm weather year-round in Southern US. Thousands
+          of miles of beaches made Florida one of the most popular tourist
+          destinations in the United States.
+        </p>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/florida/panhandle">Panhandle Beach Cams</Link>
+        </h2>
+        <p>
+          Come see the Florida Panhandle and everything it has to offer. The
+          Florida Panhandle is a great place to visit with beautiful ocean and
+          beaches and fun activities. Come and explore! Jacksonville Beach and
+          the St. Augustine Panhandle is one of the US's most beautiful places
+          to visit, come and experience it for yourself. Panhandle Beach Cams
+          provide an amazing live streaming experience to those around the world
+          who want to watch the beach life at its best.
+        </p>
+        <div className="cam-container">
+          {panhandleCams()}
+          <div className="more-cams">
+            <Link href="/florida/panhandle">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>PAN HANDLE</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/florida/northeast/">Northeast Beach Cams</Link>
+        </h2>
+        <p>
+          Northeast Florida, the Sunshine State, has many attractions. Ocean and
+          beaches are plentiful, as well as places to see and do. There is so
+          much to visit in this part of Florida. Watch live streaming videos of
+          the top beaches in Northeast Florida with an interactive map and lots
+          more information about attractions, tourist attractions and more!
+        </p>
+        <div className="cam-container">
+          {northEastCams()}
+          <div className="more-cams">
+            <Link href="/florida/oahu">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>NORTH EAST</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/florida/east-central">East Central Cams</Link>
+        </h2>
+        <p>
+          East Central Florida is a great place to visit. Ocean and beaches,
+          things to do and places to go. See Daytona Beach, Cape Canaveral,
+          Space Coast (Cape Kennedy) and New Smyrna Beach. Stay tuned to our
+          live streaming webcams and enjoy beautiful beaches.
+        </p>
+        <div className="cam-container">
+          {eastCentralCams()}
+          <div className="more-cams">
+            <Link href="/florida/east-central">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>EAST CENTRAL</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/florida/miami/">Miami Beach Cams</Link>
+        </h2>
+        <p>
+          Miami Beach Florida is the world's most famous beach resort, famed for
+          its white sand beaches and as a meeting place for celebrities and
+          millionaires. Miami Beach is also a popular tourist destination,
+          especially among Europeans and South Americans. It has been featured
+          in many movies and television shows including Scarface, Cocoon, Miami
+          Vice and Baywatch. Watch live streaming webcams of Miami.
+        </p>
+        <div className="cam-container">
+          {miamiBeachCams()}
+          <div className="more-cams">
+            <Link href="/florida/miami">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>MIAMI</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+        <h2>
+          <Link href="/florida/southeast-keys/">
+            South East Florida and The Keys Cams
+          </Link>
+        </h2>
+        <p>South East Florida and The Keys Cams.</p>
+        <div className="cam-container">
+          {souteastKeysCams()}
+          <div className="more-cams">
+            <Link href="/florida//southeast-keys">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>SOUTH EAST KEYS</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+
+        <h2>
+          <Link href="/florida/gulf-coast">Gulf Coast Beach Cams</Link>
+        </h2>
+        <p>Gulf Coast Cams.</p>
+        <div className="cam-container">
+          {gulfCoastCams()}
+          <div className="more-cams">
+            <Link href="/florida/gulf-coast">
+              <a>
+                <h2>
+                  MORE
+                  <br />
+                  <span>GULF COAST</span>
+                  <br />
+                  CAMS
+                </h2>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <AdLeaderboard />
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const moreCamsRes = await fetch(`${process.env.NEXT_PUBLIC_API}/cams/florida`)
+  const floridaCams: WebcamProps = await moreCamsRes.json()
+
+  return {
+    props: {
+      floridaCams,
+    },
+  }
+}
+
+export default Page
