37e3a6685c9da15aa1b942916f5bf8b1767e8b11
dwestland <don@westland.net>
2022-11-30 20:48:34 -0800

    Add More Cams to Webcam Page

--- pages/webcam/[...params].tsx
+++ pages/webcam/[...params].tsx
@@ -3,14 +3,20 @@ import { useSession } from 'next-auth/react'
 import Layout from '@/components/Layout'
 import Link from 'next/link'
 import { InferGetStaticPropsType } from 'next'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import AdLeaderboard from '@/components/AdLeaderboard'
 import AdLarge from '@/components/AdLarge'
 import dynamic from 'next/dynamic'
 import FlagModal from '@/components/FlagModal'
 import AdminWebcamPage from '@/components/AdminWebcamPage'
+import MoreHawaiiCams from '@/components/MoreHawaiiCams'
+
+interface WebcamProps {
+  hawaiiCams: { title: string }[]
+}
 
 const Webcam = ({
+  hawaiiCams,
   cams,
 }: InferGetStaticPropsType<typeof getServerSideProps>) => {
   const {
@@ -21,23 +27,24 @@ const Webcam = ({
     lat,
     lng,
     longDescription,
+    moreCams,
     state,
     subarea,
     title,
-    titleSlug,
+    // titleSlug,
     youtubeId,
   } = cams.cams[0]
 
   const WebcamMap = dynamic(() => import('@/components/WebcamMap'), {
     ssr: false,
   })
-  const router = useRouter()
+  // const router = useRouter()
 
   const { data: session } = useSession()
 
   const [isAdmin, setIsAdmin] = useState(false)
   const [showFlagModal, setShowFlagModal] = useState(false)
-  const { params = [] } = router.query
+  // const { params = [] } = router.query
   const youtubeUrl = `https://www.youtube.com/embed/${youtubeId}`
 
   useEffect(() => {
@@ -50,55 +57,58 @@ const Webcam = ({
   const handleFlag = () => {
     setShowFlagModal(true)
   }
+  console.log('%c  hawaiiCams ', 'background: red; color: white', hawaiiCams)
+  console.log('%c moreCams ', 'background: red; color: white', moreCams)
 
   return (
     <Layout documentTitle={title} documentDescription={description}>
       <div className="layout">
-        <div className="container">
-          <h1>{title}</h1>
-          <h2>{description}</h2>
-          <AdLeaderboard />
-          {isAdmin && (
-            <AdminWebcamPage cams={cams.cams[0]} style={{ height: '200px' }} />
-          )}
-          <div className="video-responsive">
-            <iframe
-              title="YouTube Webcam"
-              width="560"
-              height="315"
-              src={youtubeUrl}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          </div>
-          <div style={{ textAlign: 'center' }}>
-            <button
-              className="btn ghostButton"
-              type="button"
-              onClick={handleFlag}
-              style={{ margin: '15px auto' }}
-            >
-              Flag this Cam
-            </button>
-          </div>
-          <div className="content-and-ad">
-            <div className="content">
-              <WebcamMap lat={lat || 0} lng={lng || 0} />
-            </div>
-            <div className="ad">
-              <AdLarge />
-            </div>
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: longDescription }} />
-          <AdLeaderboard />
-          {/* /////////// More Cams /////////// */}
-          <h2>
-            <Link href="/hawaii/">More Hawaii Beach Cams</Link>
-          </h2>
-          {/* /////////// More Cams /////////// */}
-          <AdLeaderboard />
+        {/* <div className="container"> */}
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+        <AdLeaderboard />
+        {isAdmin && (
+          <AdminWebcamPage cams={cams.cams[0]} style={{ height: '200px' }} />
+        )}
+        <div className="video-responsive">
+          <iframe
+            title="YouTube Webcam"
+            width="560"
+            height="315"
+            src={youtubeUrl}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
         </div>
+        <div style={{ textAlign: 'center' }}>
+          <button
+            className="btn ghostButton"
+            type="button"
+            onClick={handleFlag}
+            style={{ margin: '15px auto' }}
+          >
+            Flag this Cam
+          </button>
+        </div>
+        <div className="content-and-ad">
+          <div className="content">
+            <WebcamMap lat={lat || 0} lng={lng || 0} />
+          </div>
+          <div className="ad">
+            <AdLarge />
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: longDescription }} />
+        <AdLeaderboard />
+        {/* /////////// More Cams /////////// */}
+        <h2>
+          <Link href="/hawaii/">More Hawaii Beach Cams</Link>
+        </h2>
+        <MoreHawaiiCams cams={hawaiiCams} />
+        {/* /////////// More Cams /////////// */}
+        <AdLeaderboard />
+        {/* </div> */}
       </div>
 
       {showFlagModal && (
@@ -118,8 +128,9 @@ const Webcam = ({
 
 export async function getServerSideProps(context) {
   const { params } = context.query
-  const result = ['/webcam-page?']
 
+  // Create webcam page path
+  const result = ['/webcam-page?']
   for (let i = 0; i <= params.length - 2; i++) {
     if (i === 0) {
       result.push(`country=${params[0]}`)
@@ -129,9 +140,9 @@ export async function getServerSideProps(context) {
     }
   }
   result.push(`&titleSlug=${params[params.length - 1]}`)
-
   const queryString = result.join('')
 
+  // Call webcam dynamic page
   const res = await fetch(`${process.env.NEXT_PUBLIC_API}${queryString}`)
   const cams: CamsDetailProps = await res.json()
 
@@ -142,9 +153,13 @@ export async function getServerSideProps(context) {
     }
   }
 
+  const hawaiiRes = await fetch(`${process.env.NEXT_PUBLIC_API}/cams/hawaii`)
+  const hawaiiCams: WebcamProps = await hawaiiRes.json()
+
   return {
     props: {
       cams,
+      hawaiiCams,
     },
   }
 }
