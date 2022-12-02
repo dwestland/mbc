68af6847b1fcf6f03c694e2707706d460932bfed
dwestland <don@westland.net>
2022-12-02 08:03:36 -0800

    Add protection to API routes

--- pages/api/cams/edit/index.ts
+++ pages/api/cams/edit/index.ts
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-useless-path-segments */
 import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/utils/prisma'
+import { getSession } from 'next-auth/react'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session: any = await getSession({ req })
   if (req.method !== 'PUT') {
     return res.status(405).json({ message: 'Method not allowed' })
   }
@@ -30,38 +32,44 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     webcamUrl,
   } = req.body.data
 
-  try {
-    await prisma.cams.update({
-      where: {
-        id,
-      },
-      data: {
-        area,
-        country,
-        description,
-        hidden,
-        imageName,
-        lat,
-        lng,
-        longDescription,
-        mbcHosted,
-        mbcHostedYoutube,
-        moreCams,
-        postalCode,
-        state,
-        subarea,
-        title,
-        titleSlug,
-        topCam,
-        youtubeId,
-        webcamUrl,
-      },
-    })
+  if (session?.user.role === 'ADMIN') {
+    try {
+      await prisma.cams.update({
+        where: {
+          id,
+        },
+        data: {
+          area,
+          country,
+          description,
+          hidden,
+          imageName,
+          lat,
+          lng,
+          longDescription,
+          mbcHosted,
+          mbcHostedYoutube,
+          moreCams,
+          postalCode,
+          state,
+          subarea,
+          title,
+          titleSlug,
+          topCam,
+          youtubeId,
+          webcamUrl,
+        },
+      })
 
-    res.status(200).json({ data: 'resource updated successfully' })
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({ err: 'Error occurred.' })
+      res.status(200).json({ data: 'resource updated successfully' })
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ err: 'Error occurred.' })
+    }
+  } else {
+    res.statusCode = 401
+    res.end(`Unauthorized`)
   }
+
   return null
 }
