09d87523f2515b42b52c8cfe8b3db1215c55b1e0
dwestland <don@westland.net>
2023-06-29 12:30:42 -0700

    Fix TypeScript errors

--- src/components/CamCard.tsx
+++ src/components/CamCard.tsx
@@ -13,15 +13,21 @@ interface CamCardProps {
     area: string
     country: string
     description: string
+    hidden: boolean
     id: number
     imageName: string
     lat: number
     lng: number
+    longDescription: string
     mbcHostedYoutube: boolean
+    moreCams: string
+    postalCode: string
     state: string
     subarea: string
     title: string
+    titleSlug: string
     topCam: boolean
+    youtubeId: string
     webcamUrl: string
   }
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,37 +3,49 @@ export interface CamPageProps {
 }
 
 export interface Cams {
-  id: number
-  title: string
-  webcamUrl: string
-  imageName: string
-  description: string
-  country: string
-  state: string
   area: string
-  subarea: string
+  country: string
+  description: string
+  hidden: boolean
+  id: number
+  imageName: string
   lat: number
   lng: number
+  longDescription: string
+  mbcHostedYoutube: boolean
+  moreCams: string
+  postalCode: string
+  state: string
+  subarea: string
+  title: string
+  titleSlug: string
   topCam: boolean
-  mbcHosted: boolean
+  youtubeId: string
+  webcamUrl: string
 }
 
 export interface WebcamProps {
   cams: [
     {
-      id: number
-      title: string
-      webcamUrl: string
-      imageName: string
-      description: string
-      country: string
-      state: string
       area: string
-      subarea: string
+      country: string
+      description: string
+      hidden: boolean
+      id: string
+      imageName: string
       lat: number
       lng: number
+      longDescription: string
+      mbcHostedYoutube: boolean
+      moreCams: string
+      postalCode: string
+      state: string
+      subarea: string
+      title: string
+      titleSlug: string
       topCam: boolean
-      mbcHosted: boolean
+      youtubeId: string
+      webcamUrl: string
     }
   ]
   moreCams: { title: string }[]
@@ -43,19 +55,25 @@ export interface MoreWebcamProps {
   cams: {
     cams: [
       {
-        id: number
-        title: string
-        webcamUrl: string
-        imageName: string
-        description: string
-        country: string
-        state: string
         area: string
-        subarea: string
+        country: string
+        description: string
+        hidden: boolean
+        id: string
+        imageName: string
         lat: number
         lng: number
+        longDescription: string
+        mbcHostedYoutube: boolean
+        moreCams: string
+        postalCode: string
+        state: string
+        subarea: string
+        title: string
+        titleSlug: string
         topCam: boolean
-        mbcHosted: boolean
+        youtubeId: string
+        webcamUrl: string
       }
     ]
   }
