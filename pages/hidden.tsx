800ee915ad89eaaa15e27252c1af137058ca830d
dwestland <don@westland.net>
2024-08-11 14:56:17 -0700

    Add Hidden page

---
+++ pages/hidden.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import { InferGetStaticPropsType } from 'next'
+import Layout from '@/components/Layout'
+import CamCard from '@/components/CamCard'
+import data from '@/data/camLocationAreas'
+import { getSixDigitRandom } from '@/utils/common'
+import * as types from '@/utils/types'
+
+const Hidden = ({
+  cams,
+}: InferGetStaticPropsType<typeof getServerSideProps>) => {
+  // Next modal SSR
+  // const CamsMap: any = dynamic(() => import('@/components/CamsMap'), {
+  //   ssr: false,
+  // })
+
+  const country = 'USA'
+  const state = 'Hawaii'
+  const area = 'Oahu'
+  const countryObject = data.countries.filter((ele) => ele.country === country)
+  const stateObject = countryObject[0].states.filter(
+    (ele) => ele.state === state
+  )
+  const areaObject = stateObject[0].areas.filter((ele) => ele.area === area)
+
+  const subareaObjects = areaObject[0].subareas
+  const subareaArray = subareaObjects.map((ele) => ele.subarea)
+
+  // Display cams WITH subareas
+  const camSections = subareaArray.map((subarea) => {
+    // check if subarea cams exist
+    const camCount = cams.cams
+      .map((cam: types.Cams) => cam.subarea)
+      .filter((ele) => ele === subarea).length
+    if (camCount === 0) {
+      return null
+    }
+
+    return (
+      <div key={getSixDigitRandom()}>
+        <h2>{subarea} Webcams</h2>
+        <div key={subarea} className="cam-container">
+          {cams.cams.map((cam: types.Cams) => {
+            if (cam.subarea === subarea) {
+              return <CamCard key={cam.id} cam={cam} />
+            }
+            return null
+          })}
+        </div>
+      </div>
+    )
+  })
+
+  // Display cams WITHOUT subareas
+  // const moreCams = () => {
+  //   const subareaCams = cams.cams.filter(
+  //     (cam: types.Cams) => cam.area === area && cam.subarea === ''
+  //   )
+
+  //   if (subareaCams.length === 0) {
+  //     return null
+  //   }
+
+  //   const result = subareaCams.map((cam: types.Cams) => (
+  //     <CamCard key={cam.id} cam={cam} />
+  //   ))
+
+  //   return (
+  //     <>
+  //       <AdLeaderboard />
+  //       <h2>{area} Webcams</h2>
+  //       <div className="cam-container">{result}</div>
+  //     </>
+  //   )
+  // }
+
+  // Create vectors for map
+  const vectors = []
+  cams.cams.map((cam: types.Cams) => {
+    if (cam.area === area && cam.lat !== null && cam.lng !== null) {
+      const vector = {
+        name: cam.title,
+        lat: cam.lat,
+        lng: cam.lng,
+      }
+      vectors.push(vector)
+    }
+    return null
+  })
+
+  return (
+    <Layout
+      documentTitle="Beach Cams of Oahu, Hawaii - Webcams at Waikiki Beach, Honolulu and North Shore"
+      documentDescription="Best Beach Cams and Surf Cams in Oahu, Hawaii with webcams in Waikiki Beach, Honolulu and the North Shore."
+      nofollow
+    >
+      <div className="layout">
+        <h1>Hidden Webcams</h1>
+
+        {camSections}
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/cams/hidden`)
+  const cams: types.CamPageProps = await res.json()
+
+  return {
+    props: {
+      cams,
+    },
+  }
+}
+
+export default Hidden
