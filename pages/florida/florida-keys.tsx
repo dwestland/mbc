69b01b1733eb78d97bcd30e661bd1f05820ef7dd
dwestland <don@westland.net>
2024-08-12 14:45:17 -0700

    Add copy to the Keys page

--- pages/florida/florida-keys.tsx
+++ pages/florida/florida-keys.tsx
@@ -112,35 +112,85 @@ const FloridaKeysPage = ({
             <AdLarge />
           </div>
         </div>
-        <p>
-          Southeast Florida is home to world-famous Miami Beach and West Palm
-          Beach. Fort Lauderdale was made famous for wild college Spring Break
-          getaways, but rest assured there are plenty more relaxing options
-          available in the Keys as well. See all of the great streaming live
-          Webcams in and around the Miami area, including Biscayne Bay, South
-          Beach, Key Biscayne and more.
-        </p>
+        <ShowMoreText
+          lines={2}
+          more="show more"
+          less="show less"
+          anchorClass="anchorClass"
+          truncatedEndingComponent="... "
+        >
+          <p>
+            Immerse yourself in the splendor of the Florida Keys through our
+            live webcams. From the northern shores of Key Largo to the lively
+            spirit of Key West, witness it all. Gauge the weather, map out your
+            adventure, or simply bask in the breathtaking views.
+          </p>
+          <p>
+            Key Largo teems with marinas and water escapades. Plantation Key
+            beckons with fishing and diving thrills. In Islamorada, relish
+            Robbie's Marina and its famed Tarpon feeding. Marathon enchants with
+            tranquil beaches and a kaleidoscope of marine life. Finally, Key
+            West dazzles with historic treasures and bustling streets.
+          </p>
+          <p>
+            These webcams offer you a portal to paradise. Tune in now and let
+            the Florida Keys enchant you.
+          </p>
+        </ShowMoreText>
         {camSections}
         {moreCams()}
 
         <div className="panel">
           <ShowMoreText
-            lines={3}
+            lines={4}
             more="show more"
             less="show less"
             anchorClass="anchorClass"
             truncatedEndingComponent="... "
           >
             <p>
-              If you’re looking for some fun and entertainment, there are plenty
-              of options in Miami. You can take a ride on one of the many boats
-              that go out to sea every day, or you can just stay on land and
-              enjoy the beautiful beaches. There are also several museums that
-              offer free admission, including the Holocaust Memorial Museum and
-              History Miami Museum. The best part about Miami is that it’s an
-              international city, so you can experience new cultures while
-              you’re there. There are plenty of restaurants that offer authentic
-              Cuban food or fresh seafood from the nearby waters.
+              The Florida Keys offer an endless array of experiences. From Key
+              Largo to Key West, each island holds its own allure. Our webcams
+              let you see these wonders in real-time. Whether you're plotting a
+              trip or just intrigued, these views bring the Keys to life.
+            </p>
+            <p>
+              Start in Key Largo, where the ocean beckons. Marinas here throng
+              with boats and yachts. The clear waters invite snorkeling and
+              diving. Key Largo's coral reefs are world-renowned. Watch live as
+              marine life flourishes below the surface.
+            </p>
+            <p>
+              Next, journey to Plantation Key. This island is a haven for
+              fishing enthusiasts. The weather cam unveils the pristine waters.
+              Taverns and resorts offer a taste of local culture. You might even
+              glimpse a fisherman wrestling with a big catch.
+            </p>
+            <p>
+              Islamorada is the next stop, a village of islands. It's famed for
+              its sport fishing. Robbie's Marina is a must-see. You can feed the
+              tarpon or watch others attempt it. The Cheeca Lodge webcam grants
+              a view of luxury nestled in nature. You'll see why Islamorada
+              captivates travelers.
+            </p>
+            <p>
+              Marathon is where tranquility presides. It's home to some of the
+              most serene beaches in the Keys. The Pigeon Key webcam frames the
+              iconic Seven Mile Bridge. Tranquility Bay Resort offers peaceful
+              views of white sand and palm trees. Whether it's swimming,
+              snorkeling, or sunbathing, Marathon beckons.
+            </p>
+            <p>
+              Finally, Key West calls. It's the cultural heartbeat of the Keys.
+              Live webcams spotlight Mallory Square's iconic sunsets. The
+              Southernmost Point cam marks the edge of the U.S. Key West also
+              pulses with lively streets and historic landmarks. There's always
+              something unfolding in this colorful city.
+            </p>
+            <p>
+              These webcams allow you to immerse yourself in the Florida Keys
+              from anywhere. Whether for fun or planning, they animate the
+              islands for you.
             </p>
           </ShowMoreText>
         </div>
@@ -148,13 +198,85 @@ const FloridaKeysPage = ({
           <div className="things">
             <h3>Top 10 Things to do in the Florida Keys</h3>
             <ol>
-              <li>item</li>
+              <li>Discover the vibrant coral reefs in Key Largo.</li>
+              <li>Wander through Robbie's Marina in Islamorada.</li>
+              <li>Traverse the iconic Seven Mile Bridge in Marathon.</li>
+              <li>Savor the sunset at Mallory Square in Key West.</li>
+              <li>Feed the energetic tarpon at Robbie's Marina.</li>
+              <li>Dive into the clear waters at Bahia Honda State Park.</li>
+              <li>Stand at the Southernmost Point in Key West.</li>
+              <li>Stroll through the historic district of Key West.</li>
+              <li>Unwind on the soft sands of Sombrero Beach.</li>
+              <li>Encounter marine life at the Key West Aquarium.</li>
             </ol>
           </div>
           <div className="info">
             <h3>Florida Keys Links and Local Information</h3>
             <ul>
-              <li>item</li>
+              <li>
+                <a
+                  href="https://www.fla-keys.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Florida Keys Official Tourism Site
+                </a>{' '}
+                - Navigate the ultimate guide to the Florida Keys, with events,
+                adventures, and travel insights.
+              </li>
+              <li>
+                <a
+                  href="https://www.keywest.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Key West Travel Guide
+                </a>{' '}
+                - Unearth insider tips, maps, and must-visit spots in Key West.
+              </li>
+              <li>
+                <a
+                  href="https://bahiahondapark.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Bahia Honda State Park
+                </a>{' '}
+                - Find out about details on beaches, camping, and snorkeling at
+                this premier park.
+              </li>
+              <li>
+                <a
+                  href="https://www.nps.gov/drto/index.htm"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Dry Tortugas National Park
+                </a>{' '}
+                - Plan your visit to this remote island park, including ferry
+                times and key attractions.
+              </li>
+              <li>
+                <a
+                  href="https://www.weather.gov/key/marine"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Florida Keys NOAA Weather
+                </a>{' '}
+                - Access real-time weather updates and forecasts for the Keys.
+              </li>
+              <li>
+                <a
+                  href="https://www.kwahs.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Key West Historical Society
+                </a>{' '}
+                - Journey through Key West's rich history with exhibits and
+                tours.
+              </li>
             </ul>
           </div>
         </div>
