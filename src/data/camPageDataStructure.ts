e78e4798dac2d9552838d9989958a20088d77678
dwestland <don@westland.net>
2024-08-12 21:08:05 -0700

    Create new page data structure

---
+++ src/data/camPageDataStructure.ts
@@ -0,0 +1,40 @@
+const pageStructure = {
+  PageNames: [
+    {
+      pageName: 'New York',
+      sections: [
+        { section: 'Lng Island' },
+        { section: 'Manhattan' },
+        { section: 'Staten Island' },
+      ],
+    },
+    {
+      pageName: 'Mexico',
+      sections: [{ section: 'Cancun' }, { section: 'Baja' }],
+    },
+    {
+      pageName: 'North Carolina',
+      sections: [{ section: 'Outer Banks' }, { section: 'Wilmington' }],
+    },
+    {
+      pageName: 'South Carolina',
+      sections: [{ section: 'Myrtle Beach' }, { section: 'Charleston' }],
+    },
+    {
+      pageName: 'Florida keys',
+      sections: [
+        { section: 'Key Largo' },
+        { section: 'Plantation Key' },
+        { section: 'Islamorada' },
+        { section: 'Marathon' },
+        { section: 'Big Pine Key' },
+        { section: 'Key West' },
+        { section: 'Lake Worth Inlet' },
+        { section: 'Florida Keys' },
+        { section: 'Surf' },
+      ],
+    },
+  ],
+}
+
+export default pageStructure
