846159c51879d1a8eeb1a2bdae7a9128c0ce93eb
dwestland <don@westland.net>
2024-09-07 19:56:15 -0700

    Refactor northeast page

--- pages/florida/northeast.tsx
+++ pages/florida/northeast.tsx
@@ -1,147 +1,51 @@
 import React from 'react'
-import { InferGetStaticPropsType } from 'next'
-import ShowMoreText from 'react-show-more-text'
-import dynamic from 'next/dynamic'
-import Layout from '@/components/Layout'
-import CamCard from '@/components/CamCard'
-import data from '@/data/camLocationAreas'
-import AdLeaderboard from '@/components/AdLeaderboard'
-import AdLarge from '@/components/AdLarge'
-import { getSixDigitRandom } from '@/utils/common'
-import MoreFloridaCams from '@/components/MoreFloridaCams'
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import Link from 'next/link'
+import ShowMoreText from 'react-show-more-text'
+import Layout from '@/components/Layout'
+import AdLarge from '@/components/AdLarge'
+import CamsPageMap from '@/components/CamsPageMap'
+import RenderSubareaSections from '@/components/RenderSubareaSections'
+import data from '@/data/camLocationAreas'
+import { renderError, findSubareas } from '@/utils/common'
 import * as types from '@/utils/types'
 
-const NorthEastPage = ({
+const AreaSubareaPage = ({
   cams,
-}: InferGetStaticPropsType<typeof getServerSideProps>) => {
-  // Ensure cams and cams.cams are defined
-  if (!cams || !cams.cams || cams.cams.length === 0) {
-    return (
-      <Layout
-        documentTitle="Beach Cams in Miami and South Beach Florida"
-        documentDescription="Best live web cams and surf cams at Miami Beach and South Beach in Florida."
-      >
-        <div className="layout">
-          <h1>No cams available</h1>
-        </div>
-      </Layout>
-    )
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (error) {
+    return renderError()
   }
 
-  // Next modal SSR
-  const CamsMap: any = dynamic(() => import('@/components/CamsMap'), {
-    ssr: false,
-  })
-  const floridaCams: any = cams
+  // CUSTOMIZE PAGE 1 of 5 - Add camPageTargetType
+  const camPageTargetType = 'Northeast'
 
-  const country = 'USA'
-  const state = 'Florida'
-  const area = 'Northeast'
-
-  const countryObject = data.countries.filter((ele) => ele.country === country)
-  if (countryObject.length === 0) {
-    // Handle the case where the country is not found
-    return null
-  }
-
-  const stateObject = countryObject[0].states.find(
-    (ele) => ele.state === state
-  ) || { state: '', areas: [] }
-
-  if (!stateObject.areas) {
-    stateObject.areas = []
-  }
-
-  if (!stateObject) {
-    // Handle the case where the state is not found
-    return null
-  }
-
-  const areaObject = stateObject.areas.filter((ele) => ele.area === area)
-  if (areaObject.length === 0) {
-    // Handle the case where the area is not found
-    return null
-  }
-
-  const subareaObjects = areaObject[0].subareas
-  const subareaArray = subareaObjects.map((ele) => ele.subarea)
-
-  // Display cams WITH subareas
-  const camSections = subareaArray.map((subarea) => {
-    // check if subarea cams exist
-    const camCount = cams.cams
-      .map((cam: types.Cams) => cam.subarea)
-      .filter((ele) => ele === subarea).length
-    if (camCount === 0) {
-      return null
-    }
-
-    return (
-      <div key={getSixDigitRandom()}>
-        <AdLeaderboard />
-        <h2>{subarea} Webcams</h2>
-        <div key={subarea} className="cam-container">
-          {cams.cams.map((cam: types.Cams) => {
-            if (cam.subarea === subarea) {
-              return <CamCard key={cam.id} cam={cam} />
-            }
-            return null
-          })}
-        </div>
-      </div>
-    )
-  })
-
-  // Display cams WITHOUT subareas
-  const moreCams = () => {
-    const subareaCams = cams.cams.filter(
-      (cam: types.Cams) => cam.area === area && cam.subarea === ''
-    )
-
-    if (subareaCams.length === 0) {
-      return null
-    }
-
-    const result = subareaCams.map((cam: types.Cams) => (
-      <CamCard key={cam.id} cam={cam} />
-    ))
-
-    return (
-      <>
-        <AdLeaderboard />
-        <h2>{area} Webcams</h2>
-        <div className="cam-container">{result}</div>
-      </>
-    )
-  }
-
-  // Create vectors for map
-  const vectors = []
-  cams.cams.map((cam: types.Cams) => {
-    if (cam.area === area && cam.lat !== null && cam.lng !== null) {
-      const vector = {
-        name: cam.title,
-        lat: cam.lat,
-        lng: cam.lng,
-        id: cam.id,
-        imageName: cam.imageName,
-      }
-      vectors.push(vector)
-    }
-    return null
-  })
+  const pageSections = findSubareas(data, camPageTargetType)
+  const pageSectionsArray = pageSections
+    ? pageSections.map((area: { subarea: string }) => area.subarea)
+    : []
 
   return (
+    // CUSTOMIZE PAGE 2 of 5 - Add title and description
     <Layout
-      documentTitle="Beach Cams in Northeast Florida from St. Augustine, Jacksonville Beach and Fernandina Beach"
-      documentDescription="Best web cams from Northeast Florida with surf cams at St. Augustine, Jacksonville Beach and Fernandina Beach."
+      documentTitle={`${camPageTargetType} Beach Webcams - MyBeachCams`}
+      documentDescription={`Browse beach webcams from ${camPageTargetType}, including ${pageSectionsArray.join(
+        ', '
+      )}.`}
     >
       <div className="layout">
-        <h1>Northeast Florida Webcams</h1>
+        <h1>{camPageTargetType} Beach Webcams</h1>
+        <h3 style={{ marginTop: '0' }}>
+          Featuring webcams from{' '}
+          {pageSectionsArray.slice(0, -1).join(', ') +
+            (pageSectionsArray.length > 1
+              ? ` and ${pageSectionsArray[pageSectionsArray.length - 1]}`
+              : '')}{' '}
+        </h3>
         <div className="content-and-ad">
           <div className="content">
-            <CamsMap vectors={vectors} />
+            <CamsPageMap cams={cams} />
           </div>
           <div className="ad">
             <AdLarge />
@@ -154,6 +58,7 @@ const NorthEastPage = ({
           anchorClass="anchorClass"
           truncatedEndingComponent="... "
         >
+          {/* CUSTOMIZE PAGE 3 of 5 - Add opening text ~120 words */}
           <p>
             See the allure of Northeast Florida through our live beach webcams.
             From Amelia Island to Flagler Beach, this stretch of coastline
@@ -167,69 +72,70 @@ const NorthEastPage = ({
             Florida's most captivating coastal scenes.{' '}
           </p>
         </ShowMoreText>
-        {camSections}
-        {moreCams()}
 
-        <div className="panel">
-          <ShowMoreText
-            lines={4}
-            more="show more"
-            less="show less"
-            anchorClass="anchorClass"
-            truncatedEndingComponent="... "
-          >
-            <p>
-              Immerse yourself in Northeast Florida's coastal splendors through
-              our live webcams. This region, sweeping from Amelia Island to
-              Flagler Beach, unfolds a diverse tapestry of natural beauty,
-              historic marvels, and vibrant local culture. Each location
-              encapsulates the spirit of Florida's Atlantic coast, from
-              untouched beaches to bustling piers and storied landmarks.
-            </p>
-            <p>
-              Amelia Island, a true treasure, is celebrated for its pristine
-              beaches and lush maritime forests. Here, you can witness the
-              serene dance of the waves as they caress the shore. The webcams
-              offer a live portal into this tranquil setting, allowing you to
-              bask in the peaceful aura of Amelia Island's beaches. Whether
-              you're envisioning a day of repose on the sand or a sunset wander
-              along the coast, these webcams animate it all.
-            </p>
-            <p>
-              Further south, Jacksonville Beach pulses with energy. The
-              Jacksonville Beach Pier buzzes with activity, as surfers carve
-              through the waves, anglers cast their lines, and beachgoers soak
-              in the sun. Our webcams capture this lively activity, enabling you
-              to plunge into the action from anywhere. The pier also presents
-              breathtaking sunrise vistas, perfect for early risers eager to
-              greet the day with a stunning view.
-            </p>
-            <p>
-              St. Augustine, the nation's oldest city, intertwines history with
-              modern coastal life. The webcams here unveil iconic landmarks like
-              the Castillo de San Marcos and the Bridge of Lions. You can also
-              observe the dynamic surf culture that defines this historic beach
-              town. Whether you're captivated by the past or drawn to the surf,
-              St. Augustine offers a rich tapestry of experiences.
-            </p>
-            <p>
-              Flagler Beach, at the southern fringe of our coverage, exudes a
-              relaxed, natural charm. The Flagler Beach webcam reveals a more
-              tranquil, laid-back beach scene, where nature's rhythms take
-              center stage. It's an ideal retreat for those who savor a quieter
-              coastal experience.
-            </p>
-            <p>
-              These webcams do more than just display views; they capture the
-              very essence of Northeast Florida. Whether you're plotting your
-              next adventure or indulging in a virtual getaway, our webcams
-              forge a direct connection to these captivating locations.
-            </p>
-          </ShowMoreText>
-        </div>
+        <RenderSubareaSections pageSections={pageSections ?? []} cams={cams} />
+
+        <ShowMoreText
+          lines={4}
+          more="show more"
+          less="show less"
+          anchorClass="anchorClass"
+          truncatedEndingComponent="... "
+        >
+          {/* CUSTOMIZE PAGE 4 of 5 - Add second text ~300 words, */}
+          {/* Things to Do and Links and Info */}
+          <p>
+            Immerse yourself in Northeast Florida's coastal splendors through
+            our live webcams. This region, sweeping from Amelia Island to
+            Flagler Beach, unfolds a diverse tapestry of natural beauty,
+            historic marvels, and vibrant local culture. Each location
+            encapsulates the spirit of Florida's Atlantic coast, from untouched
+            beaches to bustling piers and storied landmarks.
+          </p>
+          <p>
+            Amelia Island, a true treasure, is celebrated for its pristine
+            beaches and lush maritime forests. Here, you can witness the serene
+            dance of the waves as they caress the shore. The webcams offer a
+            live portal into this tranquil setting, allowing you to bask in the
+            peaceful aura of Amelia Island's beaches. Whether you're envisioning
+            a day of repose on the sand or a sunset wander along the coast,
+            these webcams animate it all.
+          </p>
+          <p>
+            Further south, Jacksonville Beach pulses with energy. The
+            Jacksonville Beach Pier buzzes with activity, as surfers carve
+            through the waves, anglers cast their lines, and beachgoers soak in
+            the sun. Our webcams capture this lively activity, enabling you to
+            plunge into the action from anywhere. The pier also presents
+            breathtaking sunrise vistas, perfect for early risers eager to greet
+            the day with a stunning view.
+          </p>
+          <p>
+            St. Augustine, the nation's oldest city, intertwines history with
+            modern coastal life. The webcams here unveil iconic landmarks like
+            the Castillo de San Marcos and the Bridge of Lions. You can also
+            observe the dynamic surf culture that defines this historic beach
+            town. Whether you're captivated by the past or drawn to the surf,
+            St. Augustine offers a rich tapestry of experiences.
+          </p>
+          <p>
+            Flagler Beach, at the southern fringe of our coverage, exudes a
+            relaxed, natural charm. The Flagler Beach webcam reveals a more
+            tranquil, laid-back beach scene, where nature's rhythms take center
+            stage. It's an ideal retreat for those who savor a quieter coastal
+            experience.
+          </p>
+          <p>
+            These webcams do more than just display views; they capture the very
+            essence of Northeast Florida. Whether you're plotting your next
+            adventure or indulging in a virtual getaway, our webcams forge a
+            direct connection to these captivating locations.
+          </p>
+        </ShowMoreText>
+        <hr />
         <div className="things-and-info">
           <div className="things">
-            <h3>Top 10 Things to do in Northeast Florida</h3>
+            <h3>Top 10 Things to do in {camPageTargetType}</h3>
             <ol>
               <li>Wander through Castillo de San Marcos in St. Augustine.</li>
               <li>Saunter along Jacksonville Beach Pier.</li>
@@ -244,7 +150,7 @@ const NorthEastPage = ({
             </ol>
           </div>
           <div className="info">
-            <h3>Northeast Links and Local Information</h3>
+            <h3>{camPageTargetType} Links and Local Information</h3>
             <ul>
               <li>
                 <a
@@ -339,22 +245,48 @@ const NorthEastPage = ({
       </div>
       <hr />
       <h2>
-        <Link href="/florida/">More Florida Beach Cams</Link>
-      </h2>
-      <MoreFloridaCams cams={floridaCams} />
+        <Link href="/hawaii/">More Hawaii Beach Cams</Link>
+      </h2>{' '}
+      <p style={{ textAlign: 'center' }}>
+        <span className="green-dot">&nbsp;</span>MyBeachCam hosted page
+      </p>
     </Layout>
   )
 }
 
-export async function getServerSideProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/cams/florida`)
-  const cams: types.CamPageProps = await res.json()
+export const getServerSideProps: GetServerSideProps<
+  types.CamsPageProps2
+> = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/cams-all`)
 
-  return {
-    props: {
-      cams,
-    },
+    if (!res.ok) {
+      throw new Error(`Failed to fetch, status: ${res.status}`)
+    }
+
+    let cams: types.Cams[] = await res.json()
+
+    if (!Array.isArray(cams) || cams.length === 0) {
+      throw new Error('Cams object is not valid or empty')
+    }
+
+    // CUSTOMIZE PAGE 5 of 5 - Add camPageTargetType
+    cams = cams.filter((cam) => cam.area === 'Northeast')
+
+    return {
+      props: {
+        cams,
+      },
+    }
+  } catch (error: any) {
+    console.error('Error fetching cams:', error)
+    return {
+      props: {
+        cams: [],
+        error: error.message || 'An error occurred',
+      },
+    }
   }
 }
 
-export default NorthEastPage
+export default AreaSubareaPage
