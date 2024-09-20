02837ef2945d25112ae7490835f489aab033a12c
dwestland <don@westland.net>
2024-09-19 20:05:19 -0700

    Add state pages

---
+++ pages/rhode-island.tsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import Link from 'next/link'
+import ShowMoreText from 'react-show-more-text'
+import Layout from '@/components/Layout'
+import AdLarge from '@/components/AdLarge'
+import CamsPageMap from '@/components/CamsPageMap'
+import RenderAreaSections from '@/components/RenderAreaSections'
+import data from '@/data/camLocationAreas'
+import { findAreas } from '@/utils/common'
+import * as types from '@/utils/types'
+import ErrorLoadingWebcams from '@/components/ErrorLoadingWebcams'
+
+const StateAreasPage = ({
+  cams,
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (error) {
+    return <ErrorLoadingWebcams />
+  }
+
+  // CUSTOMIZE PAGE 1 of 5 - Add camPageTargetType
+  const camPageTargetType = 'Rhode Island'
+
+  const pageAreas = findAreas(data, camPageTargetType)
+  const pageAreasArray = pageAreas
+    ? pageAreas.map((area: { area: string }) => area.area)
+    : []
+
+  return (
+    // CUSTOMIZE PAGE 2 of 5 - Add title and description
+    <Layout
+      documentTitle={`${camPageTargetType} Beach Webcams - MyBeachCams`}
+      documentDescription={`Browse beach webcams from ${camPageTargetType}, including ${pageAreasArray.join(
+        ', '
+      )}.`}
+    >
+      <div className="layout">
+        <h1>{camPageTargetType} Beach Webcams</h1>
+        <h3 style={{ marginTop: '0' }}>
+          Featuring webcams from{' '}
+          {pageAreasArray.slice(0, -1).join(', ') +
+            (pageAreasArray.length > 1
+              ? ` and ${pageAreasArray[pageAreasArray.length - 1]}`
+              : '')}{' '}
+        </h3>
+        <div className="content-and-ad">
+          <div className="content">
+            <CamsPageMap cams={cams} />
+          </div>
+          <div className="ad">
+            <AdLarge />
+          </div>
+        </div>
+        <ShowMoreText
+          lines={2}
+          more="show more"
+          less="show less"
+          anchorClass="anchorClass"
+          truncatedEndingComponent="&nbsp;&nbsp;"
+        >
+          {/* CUSTOMIZE PAGE 3 of 5 - Add opening text ~120 words */}
+          <p>xxxx</p>
+        </ShowMoreText>
+
+        <RenderAreaSections pageAreas={pageAreas ?? []} cams={cams} />
+
+        <ShowMoreText
+          lines={4}
+          more="show more"
+          less="show less"
+          anchorClass="anchorClass"
+          truncatedEndingComponent="&nbsp;&nbsp;"
+        >
+          {/* CUSTOMIZE PAGE 4 of 5 - Add second text ~300 words, */}
+          {/* Things to Do and Links and Info */}
+          <p>xxxx</p>
+        </ShowMoreText>
+        <hr />
+        <div className="things-and-info">
+          <div className="things">
+            <h3>Top 5 Things to do in {camPageTargetType}</h3>
+            <ol>
+              <li>Visit the white sand beaches with crystal clear water</li>
+            </ol>
+          </div>
+          <div className="info">
+            <h3>{camPageTargetType} Links and Local Information</h3>
+            <ul>
+              <li>
+                <a
+                  href="https://www.visittheusa.com/state/north carolina"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Visit North Carolina
+                </a>{' '}
+                - Official tourism website for North Carolina
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <h2>
+        <Link href="/">More Beach Cams</Link>
+      </h2>{' '}
+      <p style={{ textAlign: 'center' }}>
+        <span className="green-dot">&nbsp;</span>MyBeachCam hosted page
+      </p>
+    </Layout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<
+  types.CamsPageProps2
+> = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/cams-all`)
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch, status: ${res.status}`)
+    }
+
+    let cams: types.Cams[] = await res.json()
+
+    if (!Array.isArray(cams) || cams.length === 0) {
+      throw new Error('Cams object is not valid or empty')
+    }
+
+    // CUSTOMIZE PAGE 5 of 5 - Add camPageTargetType
+    cams = cams.filter((cam) => cam.state === 'Rhode Island')
+
+    return {
+      props: {
+        cams,
+      },
+    }
+  } catch (error: any) {
+    console.error('Error fetching cams:', error)
+    return {
+      props: {
+        cams: [],
+        error: error.message || 'An error occurred',
+      },
+    }
+  }
+}
+
+export default StateAreasPage
