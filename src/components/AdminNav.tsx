0b6e014286ee1895f04abebff15831dbb69eb5f5
dwestland <don@westland.net>
2024-12-14 11:31:27 -0800

    Add links to Admin Bar

--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -6,6 +6,19 @@ export default function AdminNav() {
   return (
     <div className={styles.adminNav}>
       <strong>ADMIN</strong>
+      <ul>
+        <li>
+          <Link href="/hidden">Hidden Page</Link>
+        </li>
+        <li>
+          <Link href="/youtube">YouTube Page</Link>
+        </li>
+        <li>
+          <Link href="/webcam/USA/California/Los-Angeles/youtube-test">
+            YouTube Test
+          </Link>
+        </li>
+      </ul>
       <Link href="/cams/add">
         <a className="link-as-button">Add Cam</a>
       </Link>
