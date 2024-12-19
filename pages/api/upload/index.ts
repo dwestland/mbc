5a06eabfda15fe2c24d96539f5f9a4055344222f
dwestland <don@westland.net>
2024-12-18 17:48:57 -0800

    Fix upload API TS errors

--- pages/api/upload/index.ts
+++ pages/api/upload/index.ts
@@ -1,5 +1,5 @@
-import { IncomingForm } from 'formidable'
-// import mv from 'mv'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { IncomingForm, File } from 'formidable'
 import AWS from 'aws-sdk'
 import fs from 'fs'
 
@@ -9,8 +9,8 @@ export const config = {
   },
 }
 
-export default async (req, res) => {
-  await new Promise((resolve, reject) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await new Promise<void>((resolve, reject) => {
     const form = new IncomingForm()
 
     form.parse(req, (err, fields, files) => {
@@ -22,39 +22,50 @@ export default async (req, res) => {
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       })
 
+      const file = files.file as File | File[] | undefined
+
+      if (!file) {
+        return reject(new Error('File is missing'))
+      }
+
+      const fileArray = Array.isArray(file) ? file : [file]
+      const firstFile = fileArray[0]
+
+      if (!firstFile || !firstFile.filepath || !firstFile.originalFilename) {
+        return reject(new Error('File is missing required properties'))
+      }
+
       const uploadFile = () => {
-        const fileContent = fs.readFileSync(files.file.filepath)
-        const filename: string = files.file.originalFilename
+        const fileContent = fs.readFileSync(firstFile.filepath)
+        const filename: string = firstFile?.originalFilename || ''
 
         const params = {
-          Bucket: process.env.AWS_BUCKET_NAME,
+          Bucket: process.env.AWS_BUCKET_NAME || '',
           Key: filename,
           Body: fileContent,
           ContentType: 'image/jpeg',
         }
 
-        s3.upload(params, (error, data) => {
-          if (error) {
-            console.log('Error', error)
-          } else {
-            console.log('Upload Success', data.Location)
+        s3.upload(
+          params,
+          (error: Error, data: AWS.S3.ManagedUpload.SendData) => {
+            if (error) {
+              console.log('Error', error)
+              reject(error)
+            } else {
+              console.log('Upload Success', data.Location)
+              resolve()
+            }
           }
-        })
+        )
       }
 
       uploadFile()
-
-      // // Save file locally
-      // const oldPath = files.file.filepath
-      // const newPath = `./public/webcam-images/${files.file.originalFilename}`
-
-      // mv(oldPath, newPath, (error) => {
-      //   console.log(error)
-      // })
-
-      res.status(200).json({ fields, files })
-
       return null
     })
   })
+
+  res.status(200).json({ message: 'File uploaded successfully' })
 }
+
+export default handler
