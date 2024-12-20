c34e4ea019103d812d3c75533b05d276f407d0f3
dwestland <don@westland.net>
2024-12-20 12:29:35 -0800

    Replace deprecated iframe property

--- pages/webcam/[...params].tsx
+++ pages/webcam/[...params].tsx
@@ -95,7 +95,7 @@ const Webcam = ({
             width="560"
             height="315"
             src={youtubeUrl}
-            frameBorder="0"
+            style={{ border: 'none' }}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           />
--- src/components/YouTubeCamCard.tsx
+++ src/components/YouTubeCamCard.tsx
@@ -95,7 +95,7 @@ const CamCard: FC<CamCardProps> = ({ cam }): JSX.Element => {
             height="195"
             src={`https://www.youtube.com/embed/${cam.youtubeId}`}
             title={cam.title}
-            frameBorder="0"
+            style={{ border: 'none' }}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           />
