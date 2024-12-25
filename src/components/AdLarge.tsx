7f0a0bda1edf0b21f3f04dd447c3e71cfdc38485
dwestland <don@westland.net>
2024-12-24 17:09:42 -0800

    Add Adsense

--- src/components/AdLarge.tsx
+++ src/components/AdLarge.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import styles from '@/styles/Ad.module.scss'
+// import styles from '@/styles/Ad.module.scss'
 
-const AdLeaderboard = () => <div className={styles.adLarge}>Ad Large</div>
+const AdLeaderboard = () => (
+  <ins
+    className="adsbygoogle"
+    style={{ display: 'block' }}
+    data-ad-client="ca-pub-6144099935579862"
+    data-ad-slot="8306788946"
+    data-ad-format="auto"
+    data-full-width-responsive="true"
+  />
+)
 
 export default AdLeaderboard
