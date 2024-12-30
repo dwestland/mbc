28e225cecbe81addf84ec72785577dfbcc15f0d8
dwestland <don@westland.net>
2024-12-29 16:44:45 -0800

    Add Hawaii areas components

---
+++ src/components/MoreBigIslandCams.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import CamCard from '@/components/CamCard'
+import * as types from '@/utils/types'
+
+function MoreHawaiiCams({ cams }: types.MoreWebcamProps) {
+  return (
+    <div className="cam-container">
+      {cams.cams.map(
+        (cam, idx) => idx < 7 && <CamCard key={cam.id} cam={cam} />
+      )}
+      <div className="more-cams">
+        <Link href="/hawaii/bigisland/">
+          <a>
+            <h2>
+              MORE
+              <br />
+              <span>BIG ISLAND</span>
+              <br />
+              CAMS
+            </h2>
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default MoreHawaiiCams
---
+++ src/components/MoreOahuCams.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import CamCard from '@/components/CamCard'
+import * as types from '@/utils/types'
+
+function MoreOahuCams({ cams }: types.MoreWebcamProps) {
+  return (
+    <div className="cam-container">
+      {cams.cams.map(
+        (cam, idx) => idx < 7 && <CamCard key={cam.id} cam={cam} />
+      )}
+      <div className="more-cams">
+        <Link href="/hawaii/oahu/">
+          <a>
+            <h2>
+              MORE
+              <br />
+              <span>OAHU</span>
+              <br />
+              CAMS
+            </h2>
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default MoreOahuCams
