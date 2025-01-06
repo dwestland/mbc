0bf7ef6c463782473b8d6a124a6ea59ac336bff2
dwestland <don@westland.net>
2025-01-06 12:14:21 -0800

    Update Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,219 @@
-// @ts-nocheck
-// TODO: Replace More Cams components
 import React from 'react'
-import Layout from '@/components/Layout'
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import Link from 'next/link'
-import AdLeaderboard from '@/components/AdLeaderboard'
-import MoreHawaiiCams from '@/components/MoreHawaiiCams'
-import MoreCaliforniaCams from '@/components/MoreCaliforniaCams'
-import MoreFloridaCams from '@/components/MoreFloridaCams'
 import ShowMoreText from 'react-show-more-text'
+import Layout from '@/components/Layout'
+// import AdLarge from '@/components/AdLarge'
+// import CamsPageMap from '@/components/CamsPageMap'
+// import RenderStatesSections from '@/components/RenderStatesSections'
+// import data from '@/data/camLocationAreas'
+// import { findStates } from '@/utils/common'
+import * as types from '@/utils/types'
+import ErrorLoadingWebcams from '@/components/ErrorLoadingWebcams'
+import MoreWebcams from '@/components/MoreWebcams'
+import MoreUSACams from '@/components/MoreUSACams'
+import NewMoreHawaiiCams from '@/components/NewMoreHawaiiCams'
+import NewMoreCaliforniaCams from '@/components/NewMoreCaliforniaCams'
+import NewMoreFloridaCams from '@/components/NewMoreFloridaCams'
 
-interface WebcamProps {
-  hawaiiCams: { title: string }[]
-  californiaCams: { title: string }[]
-  floridaCams: { title: string }[]
-}
+const WorldPage = ({
+  cams,
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (error) {
+    return <ErrorLoadingWebcams />
+  }
 
-const HawaiiPage = ({
-  hawaiiCams,
-  californiaCams,
-  floridaCams,
-}: WebcamProps) => (
-  <Layout
-    documentTitle="MyBeachCams.com - Webcams of Hawaii, Florida and California"
-    documentDescription="Best Web Cams and Surf Cams in Hawaii, Florida and California and and local information about Maui, Los Angles, Miami, Oahu, San Francisco, Kauai and Fort Lauderdale"
-  >
-    <div className="layout">
-      <AdLeaderboard />
-      <h1>MyBeachCams.com</h1>
+  // CUSTOMIZE PAGE 1 of 5 - Add camPageTargetType
+  const camPageTargetType = 'USA'
+
+  // const pageSections = findStates(data, camPageTargetType)
+  // const pageSectionsArray = pageSections
+  // ? pageSections.map((state: { state: string }) => state.state)
+  // : []
+  return (
+    // CUSTOMIZE PAGE 2 of 5 - Add title and description
+    <Layout
+      documentTitle="Best Webcams around the World"
+      documentDescription="Browse hundreds of beach webcams from around the world"
+    >
+      <div className="layout">
+        {/* <h1>{camPageTargetType} Beach Webcams</h1> */}
+        <h1>Best Webcams from around the World</h1>
+        <h2>MyBeachCams.com</h2>
+        {/* <h3 style={{ marginTop: '0' }}>
+          Featuring webcams from{' '}
+          {pageSectionsArray.slice(0, -1).join(', ') +
+            (pageSectionsArray.length > 1
+              ? ` and ${pageSectionsArray[pageSectionsArray.length - 1]}`
+              : '')}{' '}
+        </h3> */}
+        <div className="links-container">
+          <ul>
+            <li>
+              <Link href="/aruba/">
+                <a>Aruba</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/bermuda/">
+                <a>Bermuda</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/bonaire/">
+                <a>Bonaire</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/curacao/">
+                <a>Curacao</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/dominican-republic/">
+                <a>Dominican Republic</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/jamaica/">
+                <a>Jamaica</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/japan/">
+                <a>Japan</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/mexico/">
+                <a>Mexico</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/new-zealand/">
+                <a>New Zealand</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/sint-maarten/">
+                <a>Sint Maarten</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/st-barts/">
+                <a>St. Barts</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/taiwan/">
+                <a>Taiwan</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/thailand/">
+                <a>Thailand</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/usa/">
+                <a>United States</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="content-and-ad">
+          <div className="content">
+            <CamsPageMap cams={cams} />
+          </div>
+          <div className="ad">
+            <AdLarge />
+          </div>
+        </div> */}
+        <ShowMoreText
+          lines={2}
+          more="show more"
+          less="show less"
+          anchorClass="anchorClass"
+          truncatedEndingComponent="&nbsp;&nbsp;"
+        >
+          {/* CUSTOMIZE PAGE 3 of 5 - Add opening text ~120 words */}
+          <p>
+            We have links to over 600 Webcams in Florida, California, Hawaii and
+            all over the world. You will find webcams with live, streaming video
+            from these top resort areas. In Hawaii we bring you web cams from
+            such happening areas as Waikiki, Kaanapali, Kona, and Lahaina.
+            California webcam locations include Los Angeles, San Francisco, San
+            Diego and the Central Coast. Also from Florida at Miami, Fort
+            Lauderdale, West Palm Beach and Key Largo. Along with the web cams,
+            there are comprehensive travel information to give you the scoop on
+            the area's main attractions and top sights, including hard-to-find
+            local links. The world's best beaches are just a click away.
+          </p>
+          <p>
+            Watch the best Hawaii Beach Cams, featuring live webcams and surf
+            cams from Waikiki, Oahu, Lahaina, Maui and all of the top resort
+            areas. We also give you comprehensive travel tips, local
+            information, maps and links. Enjoy the <em>Hawaii Beach Cams!</em>
+          </p>
+        </ShowMoreText>
+
+        {/* <h1>Cams displayed here</h1> */}
+        {/* <RenderStatesSections pageSections={pageSections ?? []} cams={cams} /> */}
+      </div>
+      <hr />
       <h2>
-        Live Webcams from <Link href="/hawaii/">Hawaii</Link>
-        ,&nbsp;
-        <Link href="/california/">California</Link>
-        &nbsp;and&nbsp;
-        <Link href="/florida/">Florida</Link>
+        <Link href="/hawaii/">Check out these Webcams</Link>
       </h2>
-
-      <ShowMoreText
-        lines={3}
-        more="show more"
-        less="show less"
-        anchorClass="anchorClass"
-        truncatedEndingComponent="&nbsp;&nbsp;"
-      >
-        <p>
-          We have links to over 300 Beach Cams in Florida, California and
-          Hawaii. You will find webcams with live, streaming video from these
-          top resort areas. In Hawaii we bring you web cams from such happening
-          areas as Waikiki, Kaanapali, Kona, and Lahaina. California webcam
-          locations include Los Angeles, San Francisco, San Diego and the
-          Central Coast. Also from Florida at Miami, Fort Lauderdale, West Palm
-          Beach and Key Largo. Along with the web cams, there are comprehensive
-          travel information to give you the scoop on the area's main
-          attractions and top sights, including hard-to-find local links. The
-          world's best beaches are just a click away.
-        </p>
-        <p>
-          Watch the best Hawaii Beach Cams, featuring live webcams and surf cams
-          from Waikiki, Oahu, Lahaina, Maui and all of the top resort areas. We
-          also give you comprehensive travel tips, local information, maps and
-          links. Enjoy the <em>Hawaii Beach Cams!</em>
-        </p>
-      </ShowMoreText>
-
+      <MoreWebcams
+        cams={{
+          cams: cams.slice(0, 7),
+        }}
+      />
+      <h2>
+        <Link href="/hawaii/">USA Webcams</Link>
+      </h2>
+      <MoreUSACams
+        cams={{
+          cams: cams.filter((cam) => cam.country === 'USA').slice(0, 7),
+        }}
+      />
       <h2>
         <Link href="/hawaii/">Hawaii Beach Cams</Link>
       </h2>
-
-      <MoreHawaiiCams cams={hawaiiCams} />
-      {/* <aMapped Webcams */}
+      <NewMoreHawaiiCams
+        cams={{
+          cams: cams.filter((cam) => cam.state === 'Hawaii').slice(0, 7),
+        }}
+      />
+      <h2>
+        <Link href="/california/">California Beach Cams</Link>
+      </h2>
+      <NewMoreCaliforniaCams
+        cams={{
+          cams: cams.filter((cam) => cam.state === 'California').slice(0, 7),
+        }}
+      />
+      <h2>
+        <Link href="/florida/">Florida Beach Cams</Link>
+      </h2>
+      <NewMoreFloridaCams
+        cams={{
+          cams: cams.filter((cam) => cam.state === 'Florida').slice(0, 7),
+        }}
+      />
+      <p style={{ textAlign: 'center' }}>
+        <span className="green-dot">&nbsp;</span>MyBeachCam hosted page
+      </p>
       <ShowMoreText
-        lines={2}
+        lines={4}
         more="show more"
         less="show less"
         anchorClass="anchorClass"
         truncatedEndingComponent="&nbsp;&nbsp;"
       >
+        {/* CUSTOMIZE PAGE 4 of 5 - Add second text ~300 words, */}
+        {/* Things to Do and Links and Info */}
         <p>
           Hawaii is a tropic travel destination within the United States and is
           one of the top choices for vacations. For US travelers, you can get to
@@ -86,84 +225,58 @@ const HawaiiPage = ({
           place to visit from the comfort of your own computer.
         </p>
       </ShowMoreText>
-      <AdLeaderboard />
-      <h2>
-        <Link href="/california/">California Beach Cams</Link>
-      </h2>
-      <MoreCaliforniaCams cams={californiaCams} />
-
-      <ShowMoreText
-        lines={2}
-        more="show more"
-        less="show less"
-        anchorClass="anchorClass"
-        truncatedEndingComponent="&nbsp;&nbsp;"
-      >
-        <p>
-          California is a mostly sunny state with hundreds of miles of gorgeous
-          beaches. Known affectionately as the "Left Coast" for its liberal
-          leaning ways, it is known for anything loose in the US has a way of
-          moving to California. You don't need to live in California to enjoy
-          the beaches with the over 70 webcams on MyBeachCams.com.
-        </p>
-      </ShowMoreText>
-
-      <AdLeaderboard />
-      <h2>
-        <Link href="/florida/">Florida Beach Cams</Link>
-      </h2>
-      <MoreFloridaCams cams={floridaCams} />
-      <ShowMoreText
-        lines={2}
-        more="show more"
-        less="show less"
-        anchorClass="anchorClass"
-        truncatedEndingComponent="&nbsp;&nbsp;"
-      >
-        <p>
-          Step into Florida's vibrant world through our webcams. Unveil Miami's
-          iconic South Beach or drift along the tranquil Florida Keys. Craft
-          your travel plans as you glimpse real-time weather and surf scenes.
-          From Daytona Beachs dawning skies to Naples' peaceful Gulf waters, our
-          webcams paint vivid portraits of Floridas diverse beauty. Whether
-          youre plotting a sandy retreat or simply savoring the view, these live
-          feeds offer a dynamic window into Floridas allure. Ignite your journey
-          with just a click!
-        </p>
-      </ShowMoreText>
-
-      <AdLeaderboard />
-    </div>
-  </Layout>
-)
-
-export async function getServerSideProps() {
-  const hawaiiRes = await fetch(`${process.env.NEXT_PUBLIC_API}/cams/hawaii`)
-  const hawaiiCams: WebcamProps = await hawaiiRes.json()
-
-  const californiaRes = await fetch(
-    `${process.env.NEXT_PUBLIC_API}/cams/california`
+      <hr />
+      <div className="things-and-info">
+        <div className="things">
+          <h3>Top 10 Things to do in {camPageTargetType}</h3>
+          <ol>
+            <li>x</li>
+          </ol>
+        </div>
+        <div className="info">
+          <h3>{camPageTargetType} Links and Local Information</h3>
+          <ul>
+            <li>x</li>
+          </ul>
+        </div>
+      </div>
+    </Layout>
   )
-  const californiaCams: WebcamProps = await californiaRes.json()
+}
 
-  const floridaRes = await fetch(`${process.env.NEXT_PUBLIC_API}/cams/florida`)
-  const floridaCams: WebcamProps = await floridaRes.json()
+export const getServerSideProps: GetServerSideProps<
+  types.CamsPageProps2
+> = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/cams-all`)
 
-  return {
-    props: {
-      hawaiiCams,
-      californiaCams,
-      floridaCams,
-    },
+    if (!res.ok) {
+      throw new Error(`Failed to fetch, status: ${res.status}`)
+    }
+
+    const cams: types.Cams[] = await res.json()
+
+    if (!Array.isArray(cams) || cams.length === 0) {
+      throw new Error('Cams object is not valid or empty')
+    }
+
+    // CUSTOMIZE PAGE 5 of 5 - Add camPageTargetType
+    // cams = cams.filter((cam) => cam.country === 'USA')
+
+    return {
+      props: {
+        cams,
+      },
+    }
+  } catch (error: any) {
+    console.error('Error fetching cams:', error)
+    return {
+      props: {
+        cams: [],
+        error: error.message || 'An error occurred',
+      },
+    }
   }
 }
 
-export default HawaiiPage
-
-// const [moreCamsData, setMoreCamsData] = useState({})
-
-// useEffect(() => {
-//   fetch(`${process.env.NEXT_PUBLIC_API}/cams/hawaii`)
-//     .then((res) => res.json())
-//     .then((data) => setMoreCamsData(data))
-// }, [])
+export default WorldPage
